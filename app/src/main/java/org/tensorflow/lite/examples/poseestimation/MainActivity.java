e901cd3f2f921e63954d66402afe358e00b7a8a9
sydls25 <sydls25@naver.com>
2023-05-24 22:00:27 +0900

    fragment navigation implement

--- app/src/main/java/org/tensorflow/lite/examples/poseestimation/MainActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/poseestimation/MainActivity.java
@@ -6,6 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.tensorflow.lite.examples.poseestimation.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Toolbar tb = (Toolbar) findViewById(R.id.toolbar) ;
+        setSupportActionBar(tb) ;
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_dailylog, R.id.navigation_home, R.id.navigation_statistic)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
     public void onClick(View v){
